801c326ded91e314cfd357ae7664bed0291bae33
KunchalaRavali <145259570+KunchalaRavali@users.noreply.github.com>
2025-08-30 13:12:03 +0530

    Update RoleService.java

--- RoleService.java
+++ RoleService.java
@@ -1,8 +1,29 @@
 package com.user.app.auth_spbootapp.service;
 
+
+
+import com.user.app.auth_spbootapp.Repository.RoleRepository;
+import com.user.app.auth_spbootapp.model.Role;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class RoleService {
 
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
 }
+
+
+
