f3dadb3904f6259428dd6d5c0967f20cad8ef834
KunchalaRavali <145259570+KunchalaRavali@users.noreply.github.com>
2025-08-30 13:10:20 +0530

    Update RoleRepository.java

--- RoleRepository.java
+++ RoleRepository.java
@@ -1,9 +1,15 @@
-package com.user.app.auth_spbootapp.repository;
+package com.user.app.auth_spbootapp.Repository;
+
+
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.user.app.auth_spbootapp.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    Optional<Role> findByRole(String role);  // ✅ correct field name
 }
+
+
