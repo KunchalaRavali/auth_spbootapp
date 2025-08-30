aea5b5e1a0d57dd6b63f25c3222ae9bd321b0004
KunchalaRavali <145259570+KunchalaRavali@users.noreply.github.com>
2025-08-30 13:16:18 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,48 +1,45 @@
 package com.user.app.auth_spbootapp.controller;
 
+
 import java.util.List;
+import java.util.Set;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import com.user.app.auth_spbootapp.model.Role;
 import com.user.app.auth_spbootapp.model.User;
 import com.user.app.auth_spbootapp.service.UserService;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
-	private final UserService userService;
-	
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    private UserService userService;
 
-	@PostMapping("/register")
-	@ResponseStatus(value = HttpStatus.CREATED) //201
-	public void saveUser(@RequestBody User dto) {
-		userService.saveUser(dto);
-	}
-	
-	@GetMapping("/all")
-	@PreAuthorize("hasRole('ADMIN')")
-	@ResponseStatus(value = HttpStatus.OK) //200
-	public List<User> getUsers() {
-		return userService.getUsers();
-	}
-	
-	@GetMapping("/{id}")
-	@PreAuthorize("hasRole('USER')")
-	@ResponseStatus(value = HttpStatus.OK) //200
-	public User getUserById(@PathVariable long id) {
-		return userService.getUserById(id);
-	}
-	
+    // ✅ Create User
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    // ✅ Get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getUsers();
+    }
+
+    // ✅ Assign Role to User
+    @PostMapping("/{username}/roles/{roleName}")
+    public String assignRole(@PathVariable String username, @PathVariable String roleName) {
+        userService.assignRoleToUser(username, roleName);
+        return "Role assigned successfully";
+    }
+
+    // ✅ Get roles of a user
+    @GetMapping("/{username}/roles")
+    public Set<Role> getUserRoles(@PathVariable String username) {
+        return userService.getUserRoles(username);
+    }
 }
